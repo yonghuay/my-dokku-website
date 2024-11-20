1413fa1f8f1233f8a0bd0216e24551bded025bbe
yong-yu <yong.yu@workday.com>
2024-11-20 20:19:58 +0000

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,8 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "yonghuay", // Usually your GitHub org/user name.
   projectName: "my-dokku-website", // Usually your repo name.
-  deploymentBranch: "gh-pages",
+  //deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
